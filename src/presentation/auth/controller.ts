f9ac9d31658c0d311d6dd7bfcfaf7eb633246175
Jerson <1995jerson@gmail.com>
2024-05-30 10:47:38 -0500

    Auth Rutas y controladores

---
+++ src/presentation/auth/controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+export class AuthController {
+
+
+    constructor(){}
+
+    register = (req: Request, res: Response) => {
+
+        res.json('registerUser')
+    }
+    loginUser = (req: Request, res: Response) => {
+
+        res.json('loginUser')
+    }
+    validateEmail = (req: Request, res: Response) => {
+
+        res.json('validateEmail')
+    }
+
+}
